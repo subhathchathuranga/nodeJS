87b12e3291eb1d6e2ada7f3ca6302709e86bde46
subhathchathuranga <csubhath@gmail.com>
2023-08-04 23:02:14 +0530

    new learnings

---
+++ test/circle.js
@@ -0,0 +1,10 @@
+let r = 20;
+const PI = Math.PI;
+
+let d = 2*r;
+let C = 2*PI*r;
+let A = PI**r;
+
+console.log(`Diameter ${d.toFixed(3)}`);
+console.log(`Circumference ${C.toFixed(3)}`);
+console.log(`Area ${A}`);
---
+++ test/variable.js
@@ -0,0 +1,5 @@
+let fName = "subhath";
+let a=5;
+let b=15;
+
+console.log(`sum is ${a%b}`);
